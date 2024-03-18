7d8f4c6b1d5110e89c2a5bc54f53c258152a052f
Shenal Herath <smh231@uclive.ac.nz>
2024-03-18 21:41:11 +1300

    Bk, 1 - final scene code - not rendered

--- main.cpp
+++ main.cpp
@@ -11,31 +11,60 @@ int main()
     // World
     HitableList world;
 
-    auto materialGround = make_shared<Lambertian>(Colour(0.8, 0.8, 0.0));
-    auto materialCentre = make_shared<Lambertian>(Colour(0.1, 0.2, 0.5));
-    auto materialLeft = make_shared<Dielectric>(1.5);
-    auto materialRight = make_shared<Metal>(Colour(0.8, 0.6, 0.2), 0.0);
+    auto groundMaterial = make_shared<Lambertian>(Colour(0.5, 0.5, 0.5));
+    world.Add(make_shared<Sphere>(Point3(0, -1000, 0), 1000, groundMaterial));
 
-    world.Add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, materialGround));
-    world.Add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, materialCentre));
-    world.Add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-    world.Add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.4, materialLeft));
-    world.Add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, materialRight));
+    for ( int i = -11; i < 11; i++ ) {
+        for ( int j = -11; j < 11; j++ ) {
+            auto chooseMaterial = RandomDouble();
+            Point3 centre(i + 0.9 * RandomDouble(), 0.2, j + 0.9 * RandomDouble());
+
+            if ( (centre - Point3(4, 0.2, 0)).Length() > 0.9 ) {
+                shared_ptr<Material> sphereMaterial;
+
+                if ( chooseMaterial < 0.8 ) {
+                    // diffuse
+                    auto albedo = Colour::Random() * Colour::Random();
+                    sphereMaterial = make_shared<Lambertian>(albedo);
+                    world.Add(make_shared<Sphere>(centre, 0.2, sphereMaterial));
+                } else if ( chooseMaterial < 0.95 ) {
+                    // metal
+                    auto albedo = Colour::Random(0.5, 1);
+                    auto fuzz = RandomDouble(0, 0.5);
+                    sphereMaterial = make_shared<Metal>(albedo, fuzz);
+                    world.Add(make_shared<Sphere>(centre, 0.2, sphereMaterial));
+                } else {
+                    // glass
+                    sphereMaterial = make_shared<Dielectric>(1.5);
+                    world.Add(make_shared<Sphere>(centre, 0.2, sphereMaterial));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<Dielectric>(1.5);
+    world.Add(make_shared<Sphere>(Point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<Lambertian>(Colour(0.4, 0.2, 0.1));
+    world.Add(make_shared<Sphere>(Point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<Metal>(Colour(0.7, 0.6, 0.5), 0.0);
+    world.Add(make_shared<Sphere>(Point3(4, 1, 0), 1.0, material3));
 
     Camera cam;
 
     cam.aspectRatio = 16.0 / 9.0;
-    cam.imageWidth = 400;
-    cam.samplesPerPixel = 100;
-    cam.maxDepth = 10;
+    cam.imageWidth = 1200;
+    cam.samplesPerPixel = 500;
+    cam.maxDepth = 6;
 
     cam.verticalFOV = 20;
-    cam.lookFrom = Point3(-2, 2, 1);
-    cam.lookAt = Point3(0, 0, -1);
+    cam.lookFrom = Point3(13, 2, 3);
+    cam.lookAt = Point3(0, 0, 0);
     cam.vecUp = Vec3(0, 1, 0);
 
-    cam.defocusAngle = 10.0;
-    cam.focusDistance = 3.4;
+    cam.defocusAngle = 0.6;
+    cam.focusDistance = 10.0;
 
     cam.Render(world);
 
