61430c45aad5804788929985e496136fce413ed8
Shenal Herath <smh231@uclive.ac.nz>
2024-03-21 17:41:42 +1300

    trilinear interpolation implemented

--- perlin.h
+++ perlin.h
@@ -34,6 +34,21 @@ private:
         }
     }
 
+    static double TrilinearInterpolation(double c[2][2][2], double u, double v, double w)
+    {
+        auto accumulation = 0.0;
+        for ( int i = 0; i < 2; i++ ) {
+            for ( int j = 0; j < 2; j++ ) {
+                for ( int k = 0; k < 2; k++ ) {
+                    accumulation += (i * u + (1 - i) * (1 - u)) *
+                                    (j * v + (1 - j) * (1 - v)) *
+                                    (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+                }
+            }
+        }
+        return accumulation;
+    }
+
 public:
     Perlin()
     {
@@ -57,11 +72,25 @@ public:
 
     double Noise(const Point3& p) const
     {
-        auto i = static_cast<int>(4 * p.X()) & 255;
-        auto j = static_cast<int>(4 * p.Y()) & 255;
-        auto k = static_cast<int>(4 * p.Z()) & 255;
+        auto u = p.X() - floor(p.X());
+        auto v = p.Y() - floor(p.Y());
+        auto w = p.Z() - floor(p.Z());
 
-        return randomFloat[permutationX[i] ^ permutationY[j] ^ permutationZ[k]];
+        auto i = static_cast<int>(floor(p.X()));
+        auto j = static_cast<int>(floor(p.Y()));
+        auto k = static_cast<int>(floor(p.Z()));
+        double c[2][2][2];
+
+        for ( int di = 0; di < 2; di++ ) {
+            for ( int dj = 0; dj < 2; dj++ ) {
+                for ( int dk = 0; dk < 2; dk++ ) {
+                    c[di][dj][dk] = randomFloat[permutationX[(i + di) & 255] ^
+                                                permutationY[(j + dj) & 255] ^
+                                                permutationZ[(k + dk) & 255]];
+                }
+            }
+        }
+        return TrilinearInterpolation(c, u, v, w);
     }
 };
 
