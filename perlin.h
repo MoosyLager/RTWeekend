9bd910e074a628d38d173e49f6b69fc34869fc6d
Shenal Herath <smh231@uclive.ac.nz>
2024-03-21 17:43:43 +1300

    hermitian smoothing implemented

--- perlin.h
+++ perlin.h
@@ -75,6 +75,9 @@ public:
         auto u = p.X() - floor(p.X());
         auto v = p.Y() - floor(p.Y());
         auto w = p.Z() - floor(p.Z());
+        u = u * u * (3 - 2 * u);
+        v = v * v * (3 - 2 * v);
+        w = w * w * (3 - 2 * w);
 
         auto i = static_cast<int>(floor(p.X()));
         auto j = static_cast<int>(floor(p.Y()));
