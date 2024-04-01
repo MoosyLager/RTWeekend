eebacafedaa4dfea9af25411a33852bd9d99c283
Shenal Herath <smh231@uclive.ac.nz>
2024-04-01 19:03:33 +1300

    bk 3 - ch 2.2 - complete

--- pi.cpp
+++ pi.cpp
@@ -8,12 +8,17 @@
 int main()
 {
     int N = 100000;
+    int runs = 0;
     int insideCircle = 0;
-    for ( int i = 0; i < N; i++ ) {
+    std::cout << std::fixed << std::setprecision(12);
+    while ( true ) {
+        runs++;
         auto x = RandomDouble(-1, 1);
         auto y = RandomDouble(-1, 1);
         if ( x * x + y * y < 1 ) insideCircle++;
+
+        if ( runs % 100000 == 0 ) {
+            std::cout << "Estimate of PI = " << (4.0 * insideCircle) / runs << "\n";
+        }
     }
-    std::cout << std::fixed << std::setprecision(12);
-    std::cout << "Estimate of PI = " << (4.0 * insideCircle) / N << "\n";
 }
