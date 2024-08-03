8892e4744bdc986727dd40231605f455330ed5bb
Shenal Herath <smh231@uclive.ac.nz>
2024-08-03 14:51:20 +1200

    stratified samples to improve pi estimation

--- pi.cpp
+++ pi.cpp
@@ -7,18 +7,27 @@
 
 int main()
 {
-    int N = 100000;
-    int runs = 0;
-    int insideCircle = 0;
     std::cout << std::fixed << std::setprecision(12);
-    while ( true ) {
-        runs++;
-        auto x = RandomDouble(-1, 1);
-        auto y = RandomDouble(-1, 1);
-        if ( x * x + y * y < 1 ) insideCircle++;
 
-        if ( runs % 100000 == 0 ) {
-            std::cout << "Estimate of PI = " << (4.0 * insideCircle) / runs << "\n";
+    int insideCircle = 0;
+    int insideCircleStratified = 0;
+    int sqrtN = 1000;
+
+    for ( int i = 0; i < sqrtN; i++ ) {
+        for ( int j = 0; j < sqrtN; j++ ) {
+            auto x = RandomDouble(-1, 1);
+            auto y = RandomDouble(-1, 1);
+            if ( x * x + y * y < 1 ) insideCircle++;
+
+            x = 2 * ((i + RandomDouble()) / sqrtN) - 1;
+            y = 2 * ((j + RandomDouble()) / sqrtN) - 1;
+            if ( x * x + y * y < 1 ) insideCircleStratified++;
         }
     }
+
+    std::cout
+        << "Regular    Estimate of Pi = "
+        << (4.0 * insideCircle) / (sqrtN * sqrtN) << '\n'
+        << "Stratified Estimate of Pi = "
+        << (4.0 * insideCircleStratified) / (sqrtN * sqrtN) << '\n';
 }
